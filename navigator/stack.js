524dbf058b8ca91a8b847dd91bb70753bf822ace
gatacachorro <100207223+gatacachorro@users.noreply.github.com>
2023-12-09 15:57:21 -0300

    Add files via upload

---
+++ navigator/stack.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import BottomTabNavigator from "./tabNavigator";StoryScreen
+import  StoryScreen from "../screens/storyScreen";
+    
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: true
+      }}
+    >
+      <Stack.Screen name="Tela Inicial" component={BottomTabNavigator} />
+      <Stack.Screen name="StoryScreen" component={StoryScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
